b360ba6938f3c2ae18a4350fa8cc4e3a6862e3fd
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2018-01-12 00:21:44 -0700

    add src and build file

---
+++ deps/build.jl
@@ -0,0 +1,25 @@
+using BinDeps
+using Compat
+
+@BinDeps.setup
+
+libxfoiljl = library_dependency("libxfoiljl")
+
+installdir=joinpath(BinDeps.depsdir(libxfoiljl),"usr","lib")
+srcdir = joinpath(BinDeps.depsdir(libxfoiljl),"src","xfoil")
+
+provides(SimpleBuild,
+    (@build_steps begin
+        ChangeDirectory(srcdir)
+        `make XFOIL_LIB=libxfoiljl.so`
+        `make -j1 install INSTALL_DIR=$installdir XFOIL_LIB=libxfoiljl.so`
+    end),libxfoiljl, os = :Linux)
+
+provides(SimpleBuild,
+    (@build_steps begin
+        ChangeDirectory(srcdir)
+        `make XFOIL_LIB=libxfoiljl.dylib`
+        `make -j1 install INSTALL_DIR=$installdir XFOIL_LIB=libxfoiljl.so`
+    end),libxfoiljl, os = :Darwin)
+
+@compat @BinDeps.install Dict(:libxfoiljl => :libxfoiljl)
